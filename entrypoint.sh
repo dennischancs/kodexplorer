fab20c210c9e6fa3157e928daa45c9e7358143b4
dennischan <dennischanzh@gmail.com>
2020-08-31 14:02:56 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,5 @@ chown -R www-data:www-data /var/www/html
 chmod -R 777 /var/www/html/
 
 mkdir -p /volume2/kodexplorer/data/
-chown -R www-data:www-data /volume2/kodexplorer/data/
-chmod -R 777 /volume2/kodexplorer/data/
+chown -R www-data:www-data /volume2/kodexplorer
+chmod -R 777 /volume2/kodexplorer
