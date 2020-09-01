8d2209f36a78087d3355af81e9ae0dda5bfd131d
dennischan <dennischanzh@gmail.com>
2020-09-01 15:41:43 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-# -n 不覆盖已有配置
-cp -rn /var/www/html/data/* /koddata
+# busybox的cp -u 复制新文件
+cp -rup /var/www/html/data/* /koddata
 rm -rf /var/www/html/data
 exec darkhttpd /var/www/html/static/ariang/ --port 5218 &
 exec php -S "0.0.0.0:5210" -t /var/www/html
