983ea97dd688c55f8ba805e5bdc19a8df564d82a
dennischan <dennischanzh@gmail.com>
2020-09-02 12:11:05 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# busybox的cp -u 复制新文件
+# busybox的cp 没有-n，改成 -u 复制新文件，-p保留原目录权属
 cp -rup /var/www/html/data/* /koddata
 rm -rf /var/www/html/data
 exec darkhttpd /var/www/html/static/ariang/ --port 5218 &
